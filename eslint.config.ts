ecfe8e34c6bbe606d7dedfdee4e7eac638349e97
Anders Søgaard <9662430+andershagbard@users.noreply.github.com>
2026-04-16 16:57:13 +0200

    Use ESLint defineConfig instead of tseslint.config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,12 @@
 import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default defineConfig(
   js.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommended,
   {
     files: ['src/**/*.{ts,tsx}'],
     plugins: {
